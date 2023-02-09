ed1459df1bdcb23bb994a067620e52acd45d96dd
Jacob Mao <zijun.mao@smartnews.com>
2023-02-09 23:43:08 +0800

    refined search view

--- githubTest/githubTest/SearchView.swift
+++ githubTest/githubTest/SearchView.swift
@@ -3,17 +3,13 @@ import SwiftUI
 struct SearchView: View {
     @EnvironmentObject var viewModel: ViewModel
     var body: some View {
-        VStack (alignment: .center) {
-            HStack (alignment: .center) {
-                TextField(ViewModel.Design.SearchView.placeholder,
-                          text: $viewModel.searchText
-                )
-                .textFieldStyle(ViewModel.Design.SearchView.style)
-                .multilineTextAlignment(ViewModel.Design.SearchView.textAlignment)
-                .padding(ViewModel.Design.SearchView.padding)
-                .font(ViewModel.Design.SearchView.font)
-            }
-        }
+        TextField(ViewModel.Design.SearchView.placeholder,
+                  text: $viewModel.searchText
+        )
+        .textFieldStyle(ViewModel.Design.SearchView.style)
+        .multilineTextAlignment(ViewModel.Design.SearchView.textAlignment)
+        .padding(ViewModel.Design.SearchView.padding)
+        .font(ViewModel.Design.SearchView.font)
     }
 }
 
